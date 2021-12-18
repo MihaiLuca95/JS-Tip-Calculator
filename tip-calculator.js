1b236e0a192cdb697f34ffaee8e6a175a69d4bcc
MihaiLuca95 <91202028+MihaiLuca95@users.noreply.github.com>
2021-12-18 18:46:23 +0200

    Add files via upload

---
+++ tip-calculator.js
@@ -0,0 +1,17 @@
+// Variables
+const billAmout = document.getElementById('billAmt')
+const serviceQuality = document.querySelectorAll('input.form-check-input')
+const numberOfP = document.getElementById('nrOfPeople')
+const btn = document.getElementById('calculateBill')
+const result = document.getElementById('totalPerPerson')
+
+// Execute function when click the btn
+btn.addEventListener('click', () => {
+    let r = null;
+    [...serviceQuality].find( radio => radio.checked ? r = parseFloat(radio.value) : r) 
+
+    let resultForEach = (parseFloat(billAmout.value) * r / parseInt(numberOfP.value)).toFixed(2)
+
+    const totalAmt = (billAmout.value != '' &&  r != null) ? result.innerHTML = `$${resultForEach}` : alert("Please complete all field")
+    return totalAmt
+})
